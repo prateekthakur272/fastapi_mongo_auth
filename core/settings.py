91ccdff4ae46683745cfdb1b9f338027952ce697
Prateek Thakur <prateekthakur272@gmail.com>
2024-03-25 16:41:19 +0530

    remove db url

--- core/settings.py
+++ core/settings.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
 
-    DB_URL:str = "mongodb+srv://admin:prateek272@campus-connect-v2.cimwmpv.mongodb.net/?retryWrites=true&w=majority&appName=campus-connect-v2"
+    DB_URL:str = ""
     SECRET_KEY:str = '1eb70e9ae3586b4bcd960d662adb0c5ee4e6c6e643ef8f61909411094c4ed464'
     JWT_ALGORITHM:str = 'HS256'
     JWT_EXPIRATION_TIME:int = 30
